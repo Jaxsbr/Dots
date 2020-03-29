4f566aaeca514144f0d3e779f1a55368b245708c
jaco.brink <jaco.brink@xero.com>
2020-03-30 07:48:30 +1300

    refactor

--- client/game.js
+++ client/game.js
@@ -1,50 +1,63 @@
-var socket = io();
+Game = function() {
+  this.socket = io();
+  this.mouseData = { x: 0, y: 0 };
+  this.players = {};
+  this.dots = [];
 
-var movement = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
-}
-var mouseData = { 
-    x: 0,
-    y: 0
-}
-document.addEventListener('mousemove', function (event) {
-    mouseData.x = event.x;
-    mouseData.y = event.y;
-});
+  this.canvas = document.getElementById("canvas");
+  this.canvas.width = 600;
+  this.canvas.height = 600;
 
-socket.emit('new player');
-setInterval(function () {
-    socket.emit('movement', movement);
-    socket.emit('mouseData', mouseData);
-}, 1000 / 60);
+  this.context = this.canvas.getContext("2d");
 
-var canvas = document.getElementById('canvas');
-canvas.width = 800;
-canvas.height = 600;
-var context = canvas.getContext('2d');
-socket.on('state', function (stateData) {
-    context.clearRect(0, 0, 800, 600);
-    context.fillStyle = 'green';
+  document.addEventListener("mousemove", this.HandleMouseMove.bind(this));
 
-    for (var id in stateData.players) {
-        var player = stateData.players[id];
-        context.fillStyle = 'green';
-        context.beginPath();
-        context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
-        context.fill();
+  this.socket.emit("new player");
+  this.socket.on("state", this.HandleStateChange.bind(this));
 
-        context.font = "10px Arial";
-        context.fillText(id, player.x, player.y);
-    }
+  setInterval(this.Loop.bind(this), 1000 / 60);
+};
 
-    for (let index = 0; index < stateData.dots.length; index++) {
-        const dot = stateData.dots[index];
-        context.fillStyle = dot.color;
-        context.beginPath();
-        context.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
-        context.fill();
-    }
-});
+Game.prototype.Loop = function() {
+  this.socket.emit("mouseData", this.mouseData);
+  this.Render();
+};
+
+Game.prototype.HandleMouseMove = function(event) {
+  this.mouseData.x = event.x;
+  this.mouseData.y = event.y;
+};
+
+Game.prototype.HandleStateChange = function(stateData) {
+  this.players = stateData.players;
+  this.dots = stateData.dots;
+};
+
+Game.prototype.Render = function() {
+  this.context.clearRect(0, 0, 600, 600);
+  this.RenderPlayers();
+  this.RenderDots();
+};
+
+Game.prototype.RenderPlayers = function() {
+  this.context.fillStyle = "green";
+  for (var id in this.players) {
+    var player = this.players[id];
+    this.context.beginPath();
+    this.context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
+    this.context.fill();
+
+    this.context.font = "10px Arial";
+    this.context.fillText(id, player.x, player.y);
+  }
+};
+
+Game.prototype.RenderDots = function() {
+  for (let index = 0; index < this.dots.length; index++) {
+    const dot = this.dots[index];
+    this.context.fillStyle = dot.color;
+    this.context.beginPath();
+    this.context.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
+    this.context.fill();
+  }
+};
