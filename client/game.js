0de33d6be62ad490978fc4f66aa28637c8a2e328
jaxsbr <jbrink386@gmail.com>
2020-03-29 22:45:44 +1300

    working dots

--- client/game.js
+++ client/game.js
@@ -25,17 +25,26 @@ var canvas = document.getElementById('canvas');
 canvas.width = 800;
 canvas.height = 600;
 var context = canvas.getContext('2d');
-socket.on('state', function (players) {
-    console.log(players);
+socket.on('state', function (stateData) {
     context.clearRect(0, 0, 800, 600);
     context.fillStyle = 'green';
-    for (var id in players) {
-        var player = players[id];
+
+    for (var id in stateData.players) {
+        var player = stateData.players[id];
+        context.fillStyle = 'green';
         context.beginPath();
         context.arc(player.x, player.y, 10, 0, 2 * Math.PI);
         context.fill();
 
         context.font = "10px Arial";
         context.fillText(id, player.x, player.y);
+    }
+
+    for (let index = 0; index < stateData.dots.length; index++) {
+        const dot = stateData.dots[index];
+        context.fillStyle = dot.color;
+        context.beginPath();
+        context.arc(dot.x, dot.y, dot.size, 0, 2 * Math.PI);
+        context.fill();
     }
 });
--- server/game.js
+++ server/game.js
@@ -1,6 +1,11 @@
 module.exports = {
     start: function (io) {
+
         var players = {};
+        var dots = [];
+        var dotSpawnTick = 60;
+        var dotSpawnElapsed = 0;
+
         io.on('connection', function (socket) {
             socket.on('new player', function () {
                 players[socket.id] = {
@@ -23,8 +28,30 @@ module.exports = {
         });
 
         setInterval(function () {
-            io.sockets.emit('state', players);
+            io.sockets.emit('state', { players: players, dots: dots });
+
+            spawnDots();
+
         }, 1000 / 60);
+
+        spawnDots = function () {
+            dotSpawnElapsed++;
+            if (dotSpawnElapsed >= dotSpawnTick) {
+                dotSpawnElapsed = 0;
+
+                const size = 2;
+                const x = randomInRange(0, 600 - size);
+                const y = randomInRange(0, 600 - size);
+
+                dots.push({ x: x, y: y, size: size, color: 'red'});
+            }
+        }
+
+        randomInRange = function (min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
         getNormalizedVector = function (x1, y1, x2, y2) {
             var px = x1 - x2;
